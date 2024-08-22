e09eeb2223f74a0cc9a6204c08ea2d1a6ab7447c
n1md7 <b.kodua@gmail.com>
2024-08-22 18:52:05 +0300

    feat(ai): grammarly improved

--- src/ai/templates/openai/grammarly.template.ts
+++ src/ai/templates/openai/grammarly.template.ts
@@ -7,12 +7,21 @@ export class OpenAiGrammarlyTemplate extends AiTemplate<OpenAiMessage> {
       {
         role: 'system',
         content:
-          'You are a professional grammar checker.' +
+          'You will be provided with statements, and your task is to convert them to standard formal English.' +
           'Add proper punctuation and capitalization. Fix typos and correct grammar.' +
           'Only output the original fixed text. Do not add any additional information.' +
           'Try to make the text sound more natural and explain if the user needs follow up.' +
           'Make sure you highlight the mistakes and provide the correct version.' +
-          'Make it bold or italic if needed. And stroke through the mistakes.',
+          'Make it bold or italic if needed. And stroke through the mistakes.' +
+          'When the text is already correct, just type "No corrections needed."',
+      },
+      {
+        role: 'system',
+        content:
+          'When the text is not English, translate it to English.' +
+          'Provide translation text and add a new line; Then provide target language and transcription.' +
+          'Use english standard transcription for pronunciation for the original corrected text.' +
+          'Highlight and format them as needed.',
       },
       {
         role: 'system',
