6a6aeb44b802c0b196056ada8f54e7c12b39a9d3
n1md7 <b.kodua@gmail.com>
2024-09-19 16:37:12 +0300

    fix: config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,24 @@
 module.exports = {
-  apps : [{
-    name: 'Discord Bot (Pico)',
-    script: 'bun run start',
-    watch: false,
-    version: '1.0.0',
-    namespace: 'Discord'
-  }],
+  apps: [
+    {
+      name: 'Discord Bot (Pico)',
+      script: 'bun run start',
+      watch: false,
+      version: '1.0.0',
+      namespace: 'Discord',
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'nimda',
-      host : '192.168.116.137',
-      ref  : 'origin/master',
-      repo : 'https://github.com/n1md7-th/discord-bot.git',
-      path : '/home/nimda/apps/discord-bot',
-      'pre-deploy-local': '',
-      'post-deploy' : 'bun i && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  deploy: {
+    production: {
+      user: 'nimda',
+      host: '192.168.116.137',
+      ref: 'origin/master',
+      repo: 'https://github.com/n1md7-th/discord-bot.git',
+      path: '/home/nimda/apps/discord-bot',
+      'pre-deploy-local': 'bun i',
+      'post-deploy': 'pm2 reload ecosystem.config.cjs --env production',
+      'pre-setup': '',
+    },
+  },
 };
